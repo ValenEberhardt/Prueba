19ef5be4a05f1ff38f1864f108ee6cb602e9effc
ValenEberhardt <valentin_eberhardt@hotmail.com>
2025-08-06 12:33:45 -0300

    Dean Martin agregado

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,4 +1,8 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
 
-print("Hola mundo!")
+lista = ["every", "body", "finds", "somebody", "somtimeee"]
+
+for verso in lista:
+    print (verso)
+
